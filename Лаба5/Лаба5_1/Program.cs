03e3b3af886bd004fd87fefc4d2696a8a730d95a
eak94 <eak94@tpu.ru>
2023-11-19 23:55:48 +0700

    feature(Лаба5): добавлено два новых проекта
    -проект Лаба5 проверяет правильность написания
    -проект Лаба52 производит поиск по условию

---
+++ Лаба5/Лаба5_1/Program.cs
@@ -0,0 +1,126 @@
+/*1.	Написать программу, которая вводит текст, состоящий из нескольких предложений,
+ * и выводит его на экран меняя местами каждые два соседних слова. 
+ Дан текст. Составить программу проверки правильности написания сочетаний
+«жи», «ши», «ча», «ща», «чу» и «щу». Исправить ошибки. */
+
+namespace Laba51
+{
+    /// <summary>
+    /// Программа.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Метод выводит на консоль текст, состоящий из нескольких предложений с обменом местами каждого соседнего слово и исправляет ошибки правописания.
+        /// </summary>
+        public static void Main()
+        {
+            string textByUser = new(Console.ReadLine());
+            Console.WriteLine("Пункт 5.1.1");
+            ExchangePlaces(textByUser);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Пункт 5.1.2");
+            int count = NumberOfErrors(textByUser);
+            Console.WriteLine($"Количество ошибок в вашем тексте: {count}");
+            Console.WriteLine("Исправленный текст");
+            Console.WriteLine(ReplaceCombinationsTwo(textByUser));
+            Console.WriteLine();
+            Console.WriteLine(Places(textByUser));
+
+        }
+        /// <summary>
+        /// Программа.
+        /// </summary>
+        /// <param name="textByUser"></param>
+        /// <returns></returns>
+        public static int Places(string textByUser)
+        {
+            string[] array = textByUser.Split(' ');
+            return array.Length - 1;
+        }
+        /// <summary>
+        /// Функция, которая производит обмен местами соседних слов.
+        /// </summary>
+        /// <param name="textByUser">Текст, введенный пользователем в консоль.</param>
+        public static void ExchangePlaces(string textByUser)
+        {
+            //создаем пустой список 
+            List<string> strSplitted = new List<string>();
+            // заполняем список 
+            foreach (string word in textByUser.Split(' ', '.', ',', '!', '?', ';', ':'))
+            {
+                //исключаем двойные пробелы
+                if (word.Length != 0)
+                {
+                    strSplitted.Add(word);
+                }
+            }
+            // преобразуем список в массив 
+            string[] array = strSplitted.ToArray();
+            Console.WriteLine("Измененный текст");
+            string temp;
+            // цикл для обмена местами элементов, если честное количество
+            for (int i = 0; i < array.Length - 1; i += 2)
+            {
+                for (int j = i; j < i + 1; j++)
+                {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+            //если нечетное количество
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+        /// <summary>
+        /// Функция, для поиска количества ошибок(проверки правильности написания слов).
+        /// </summary>
+        /// <param name="textByUser">Введенный пользователем текст.</param>
+        /// <returns>Значение количества ошибок.</returns>
+        public static int NumberOfErrors(string textByUser)
+        {
+            //создаем массив с ошибками
+            string[] combinations = { "жы", "Жы", "шы", "Шы", "чя", "Чя", "щя", "Щя", "Чю", "чю", "щю", "Щю" };
+            //задаем счетчик 
+            int count = 0;
+            // цикл для поиска количества ошибок в введеном тексте 
+            foreach (string combination in combinations)
+            {
+                /*Находит первое вхождение заданной подстроки или символа в строке. Если
+                искомый символ или подстрока не обнаружены,то возвращается значение -1 */
+
+                int index = textByUser.IndexOf(combination);
+
+                while (index != -1)
+                {
+                    count++;
+                    /*с указанием начального индекса index + combination.Length.
+                     * Это позволяет исключить повторное совпадение с уже найденными вхождениями.*/
+                    index = textByUser.IndexOf(combination, index + combination.Length);
+                }
+            }
+            return count;
+        }
+        /// <summary>
+        /// Функция для исправления ошибок в тексте.
+        /// </summary>
+        /// <param name="textByUser">Текст, введенный в консоль пользователем.</param>
+        /// <returns>Исправленный текст.</returns>
+        public static string ReplaceCombinationsTwo(string textByUser)
+        {
+            string[] textByUserToReplace = { "жы", "Жы", "шы", "Шы", "чя", "Чя", "щя", "Щя", "щю", "Щю", "чю", "Чю" };
+            string[] replacements = { "жи", "Жи", "ши", "Ши", "ча", "Ча", "ща", "Ща", "щу", "Щу", "чу", "Чу" };
+
+            for (int i = 0; i < textByUserToReplace.Length; i++)
+            {
+                textByUser = textByUser.Replace(textByUserToReplace[i], replacements[i]);
+            }
+
+            return textByUser;
+        }
+    }
+}
---
+++ Лаба5/Лаба5_2/Program.cs
@@ -0,0 +1,89 @@
+/*1.Дан текст, имеющий структуру «Фамилия И.О. – рост см – вес кг». Вывести на экран фамилии всех лиц, чей рост превышает 190 см. 
+2.	Дан текст. Найти число вхождение в данный текст предлога «не».*/
+
+
+using System.Text.RegularExpressions;
+
+namespace Laba52
+{
+    /// <summary>
+    /// Выполнение лабораторной 5.2.1 и 5.2.2.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Метод, который выводит на экран фамилии всех лиц, чей рост превышает 190 см и число вхождение в данный текст предлога «не».
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Первое задание");
+            Console.WriteLine("Вид записа: Фамилия И.О. – рост см – вес кг ");
+            string textByUser = "Иванов И.О. – 180 см – 90 кг " +
+                "Петров И.О. – 170 см – 60 кг " +
+                "Сидоров И.О. – 190 см – 100 кг " +
+                "Наумкин И.О. – 189 см – 35 кг " +
+                "Папочкин И.О. – 191 см – 68 кг ";
+            Console.WriteLine(textByUser);
+            Height(textByUser);
+            Console.WriteLine();
+            Console.WriteLine("Второе задание");
+            Console.WriteLine("Предложения с не");
+            string text = new(Console.ReadLine());
+            WordNo(text);
+        }
+
+        /// <summary>
+        /// Функция, выводит на экран фамилии всех лиц, чей рост превышает 190 см.
+        /// </summary>
+        /// <param name="textByUser">Текст, введенный в консоль.</param>
+        public static void Height(string textByUser)
+        {
+            string pattern = (@"(\S+\s\S+\.)\s–\s(\d+)\sсм");
+            // Метод возвращает все вхождения регулярного выражения 
+            MatchCollection matches = Regex.Matches(textByUser, pattern);
+            //цикл по нахождению фамилии, если рост больше 190
+            List<string> strregex = new List<string>();
+            foreach (Match match in matches)
+            {
+                //присваивается результат поиска по имени. Groups[1].Value - значение в первой скобке регулярки
+                string name = match.Groups[1].Value;
+                //присваивается результат поиска по росту. Groups[2].Value - значение во второй скобке регулярки
+                // конвертируется в целое число
+                int height = int.Parse(match.Groups[2].Value);
+                // условия нахождения 
+                if (height >= 190)
+                {
+                    strregex.Add(name);
+                    Console.WriteLine($"{name}");
+                }
+            }
+            if (strregex.Count == 0)
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+        }
+        /// <summary>
+        /// Фунция для поиска числа вхождение в данный текст предлога «не».
+        /// </summary>
+        /// <param name="text">Текст, введенный в консоль. </param>
+        public static void WordNo(string text)
+        {
+            string pattern = (@"\s*не\s*");
+            MatchCollection matches = Regex.Matches(text, pattern);
+            if (matches.Count > 0)
+            {
+                // Перебор элементов коллекции
+                foreach (Match i in matches)
+                {
+                    Console.WriteLine($"Количество вхождений предлога {i}:");
+                    Console.WriteLine(matches.Count);
+                }
+            }
+            // Если объектов нет,то пользователю сообщается об отсутствии совпадений
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+        }
+    }
+}
